b7f7485cd0ab8ec40be67b710900db848cf57c32
tony <tony@bit-monkeys.com>
2015-01-04 22:52:58 -0500

    Initial Django Project Commit
    Vanilla Django Commit. I’ll work on building out associations and
    models. I’ll update the wiki when I’m done to display active field
    variables.

---
+++ openfaqs/webapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+
+class UserStettings(models.Model):
+
+class Question(models.Model):
+
+class Tag(models.Model):
+
+class QuestionTag(models.Model):
+
+class Answers(models.Model):
+
+class Comments(models.Model):
+
+class SiteSettings(models.Model):
+
+
+

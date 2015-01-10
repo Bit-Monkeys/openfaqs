eb9ea3e07961d9d9e63ec4becacf2f22d5e320bb
tony <tony@bit-monkeys.com>
2015-01-09 21:47:27 -0500

    DB Schema Done
    Drunk Tony and Lev made this work

--- openfaqs/webapp/admin.py
+++ openfaqs/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from webapp.models import *
 # Register your models here.
+class QuestionInline(admin.TabularInline):
+	model = Question
+admin.site.register(Question)
+	

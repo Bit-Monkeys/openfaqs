30334b12ee9eb2fb0b70f7ae6b55c71441032db7
Lev Lazinskiy <llazinskiy@linode.com>
2015-01-10 02:34:31 -0500

    added Badge table to models

--- openfaqs/webapp/models.py
+++ openfaqs/webapp/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class User(models.Model):
 	UserName = models.CharField(max_length=50)
 	FirstName = models.CharField(max_length=50)
@@ -17,8 +15,12 @@ class UserSettings(models.Model):
 		
 class UserBadge(models.Model):
 	UserID = models.ForeignKey(User)
-	BadgeID = models.IntegerField()	
+	BadgeID = models.ForeignKey(Badge)
 
+class Badge(models.Model):
+	Description = models.CharField(max_length=100)
+	FilePath = models.CharField(max_length=100)
+	
 class Question(models.Model):
 	UserID= models.ForeignKey(User)
 	Title = models.CharField(max_length=200)
@@ -26,13 +28,9 @@ class Question(models.Model):
 	CreatedTime = models.DateTimeField()
 	ModifiedTime = models.DateTimeField()
 	
-class Tag(models.Model):
-	Tag = models.CharField(max_length=50)	
-	
 class QuestionTag(models.Model):
 	QuestionID=models.ForeignKey(Question)
 	TagID = models.ForeignKey(Tag)
-	#association Table
 
 class QuestionComment(models.Model):
 	QuestionID = models.ForeignKey(Question)
@@ -40,7 +38,19 @@ class QuestionComment(models.Model):
 	Comment = models.TextField()
 	CreatedTime = models.DateTimeField()
 	ModifiedTime = models.DateTimeField()
-	
+
+class QuestionVote(models.Model):
+	UserID = models.ForeignKey(User)
+	QuestionID = models.ForeignKey(Question)
+	Value = models.IntegerField()
+
+class QuestionAttachment(models.Model):
+	QuestionID = models.ForeignKey(Question)
+	FilePath = models.CharField(max_length=100)
+
+class Tag(models.Model):
+	Tag = models.CharField(max_length=50)	
+
 class Answer(models.Model):
 	QuestionID = models.ForeignKey(Question)
 	UserID = models.ForeignKey(User)
@@ -48,6 +58,15 @@ class Answer(models.Model):
 	CreatedTime = models.DateTimeField()
 	ModifiedTime = models.DateTimeField()
 	
+class AnswerAttachment(models.Model):
+	AnswerID = models.ForeignKey(Answer)
+	FilePath = models.CharField(max_length=100)
+
+class AnswerVote(models.Model):
+	UserID = models.ForeignKey(User)
+	AnswerId = models.ForeignKey(Answer)
+	Value = models.IntegerField()
+
 class Comment(models.Model):
 	AnswerID = models.ForeignKey(Answer)
 	UserID = models.ForeignKey(User)
@@ -58,21 +77,3 @@ class Comment(models.Model):
 class SiteSettings(models.Model):
 	isPublic = models.BooleanField(default=True)
 	
-
-class QuestionAttachment(models.Model):
-	QuestionID = models.ForeignKey(Question)
-	FilePath = models.CharField(max_length=100)
-	
-class AnswerAttachment(models.Model):
-	AnswerID = models.ForeignKey(Answer)
-	FilePath = models.CharField(max_length=100)
-
-class QuestionVote(models.Model):
-	UserID = models.ForeignKey(User)
-	QuestionID = models.ForeignKey(Question)
-	Value = models.IntegerField()
-
-class AnswerVote(models.Model):
-	UserID = models.ForeignKey(User)
-	AnswerId = models.ForeignKey(Answer)
-	Value = models.IntegerField()

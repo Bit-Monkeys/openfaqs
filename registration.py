203dc915ac706be561c3211179a098c0c589b5a6
Lev Lazinskiy <llazinskiy@linode.com>
2015-01-20 17:53:12 -0500

    Added routes and registration fucntion;

---
+++ registration.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, render_template
+from models import db, User 
+from sessions import login 
+from utilities import hash_password
+
+import datetime 
+
+registration = Blueprint('registration', __name__, template_folder='templates')
+
+
+@registration.route('/register', methods=['GET', 'POST']) 
+def register(): 
+	if request.method == 'POST': 
+		password = hash_password(request.form['password']) 
+
+		new_user = User(request.form['firstname'],
+			request.form['lastname'], 
+			request.form['username'], 
+			request.form['email'], 
+			password, 
+			datetime.datetime.now(), 
+			datetime.datetime.now(), 
+			True)
+
+		db.session.add(new_user)
+		db.session.commit() 
+
+		login()
+
+		return render_template('dashboard.html') 	
+
+	else: 
+		return render_template('register.html') 
---
+++ sessions.py
@@ -0,0 +1,42 @@
+from flask import Blueprint 
+from models import User 
+from utilities import hash_password
+
+sessions = Blueprint('sessions', __name__, template_folder='templates') 
+
+def check_login():
+	try: 
+		if session['username']: 
+			return True 
+	except: 
+		return False 
+
+@sessions.route('/login', methods=['POST', 'GET']) 
+def login(): 
+	if request.method == 'POST': 
+
+		password_hashed = hash_password(request.form['password']) 
+		find_user = User.query.filter_by(username=request.form['username'],
+			password=password_hashed).first() 
+
+		if find_user: 
+			session['user_id'] = find_user.id 
+			session['username'] = find_user.username 
+			session['email'] = find_user.email
+			
+			return redirect('/dashboard') 
+
+		else: 
+
+			return render_template('error.html', error="Invalid credentials, please try again.") 
+
+	else: 
+		render_template('error.html') 
+
+@sessions.route('/logout', methods=['GET'])
+def logout(): 
+	session.pop('user_id', None) 
+	session.pop('username', None) 
+	session.pop('email', None)
+
+	return render_template('index.html', message="You have been logged out.") 

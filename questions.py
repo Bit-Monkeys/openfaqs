08c13948b02c0292ca27f50224821960b3610d2e
Joe Pettit <jpettit@pettitservers.com>
2015-02-03 22:22:51 -0600

    Moved decorator to utilities, removed incorrect decorator in questions and moved to ask

--- questions.py
+++ questions.py
@@ -1,12 +1,10 @@
 from flask import render_template, request, Blueprint, session
 from models import User, Tag, Question, db 
 import datetime 
-from decorator import login_required
 
 questions = Blueprint('questions', __name__, template_folder='templates') 
 
 @questions.route('/questions/<question_id>')
-@login_required
 def show_question(question_id): 
 	question = Question.query.filter_by(ID=question_id).first() 
 	user = User.query.filter_by(ID = question.UserID).first() 
